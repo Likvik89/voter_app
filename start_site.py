c2aa3c7870199b734ae53e387512a7160a14838e
Likvik <erik@elfisk.dk>
2025-05-07 10:35:26 +0200

    startn't

---
+++ start_site.py
@@ -0,0 +1,18 @@
+# pip install Flask
+from flask import Flask, render_template, request, redirect
+#from app.config import Config
+
+app = Flask(__name__)
+#app.config.from_object(Config)
+
+@app.route('/')
+def home():
+    # Her kan fx hentes data og sættes ind i html-koden
+    txt = "Jinja and Flask"
+    return render_template('index.html', title=txt)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    #app.run(debug=True) #Koer kun paa localhost
+    app.run(host='0.0.0.0', port=5000)
