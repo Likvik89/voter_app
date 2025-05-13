cc9b7e54dff4e1380db0f3aa7039068b719d753c
Likvik <erik@elfisk.dk>
2025-05-13 08:40:34 +0200

    r

--- app/app.py
+++ app/app.py
@@ -1,3 +1,5 @@
+
+
 # pip install Flask
 from flask import Flask, render_template, request, redirect
 #from app.config import Config
@@ -17,5 +19,4 @@ def q1():
 
 if __name__ == '__main__':
     app.debug = True
-    #app.run(debug=True) #Koer kun paa localhost
     app.run(host='0.0.0.0', port=5000)
