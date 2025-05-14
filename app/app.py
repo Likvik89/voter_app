eb74f77ecbf31ffe69e70f555ff5f9228600e231
Likvik <erik@elfisk.dk>
2025-05-14 10:43:47 +0200

    tr

--- app/app.py
+++ app/app.py
@@ -2,7 +2,13 @@
 from flask import Flask, render_template, request, redirect
 #from app.config import Config
 
+import sqlite3
+
+from forms import Q1_Form
+
 app = Flask(__name__)
+
+db = 'app\questions.db'
 #app.config.from_object(Config)
 
 @app.route('/')
@@ -10,10 +16,33 @@ def home():
     # Her kan fx hentes data og sættes ind i html-koden
     return render_template('index.html')
 
-@app.route('/q1/')
+"""
+@app.route('/q1/', methods=['POST', 'GET'])
 def q1():
-    txt = "Har du nogen sinde spildt vand?"
-    return render_template('q1.html', title=txt)
+    q1_form = Q1_Form()
+    if q1_form.validate_on_submit():
+        if q1_form.valg.data:
+            conn = sqlite3.connect(db)
+            cursor = conn.cursor()
+            valg = q1_form.valg.data
+            print(valg)
+            cursor.execute('INSERT INTO driver(result) VALUES ('+valg+')')
+            conn.commit()
+            conn.close()
+            return redirect('/')
+    return render_template('q1.html', q1_form = q1_form)
+"""
+
+
+@app.route('/q1/', methods=['POST', 'GET'])
+def q1():
+    q1_form = Q1_Form()
+    if q1_form.validate_on_submit():
+        if request:
+            pass
+
+    return render_template('q1.html', q1_form = q1_form)
+
 
 if __name__ == '__main__':
     app.debug = True
