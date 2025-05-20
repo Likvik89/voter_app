81b729d356fc42b8be0ac272bffed93afb0d88b8
Likvik <erik@elfisk.dk>
2025-05-20 08:42:28 +0200

    restructering

--- start_site.py
+++ start_site.py
@@ -1,22 +1,21 @@
-# pip install Flask
 from flask import Flask, render_template, request, redirect
-#from app.config import Config
-
 import sqlite3
-
-from forms import Q1_Form
+from app.config import Config
+from app.forms import Q1_Form
 
 app = Flask(__name__)
+db = 'questions.db'
+app.config.from_object(Config)
 
-db = 'app\questions.db'
-#app.config.from_object(Config)
+
 
 @app.route('/')
 def home():
     # Her kan fx hentes data og sættes ind i html-koden
     return render_template('index.html')
 
-"""
+
+
 @app.route('/q1/', methods=['POST', 'GET'])
 def q1():
     q1_form = Q1_Form()
@@ -31,17 +30,7 @@ def q1():
             conn.close()
             return redirect('/')
     return render_template('q1.html', q1_form = q1_form)
-"""
 
-
-@app.route('/q1/', methods=['POST', 'GET'])
-def q1():
-    q1_form = Q1_Form()
-    if q1_form.validate_on_submit():
-        if request:
-            pass
-
-    return render_template('q1.html', q1_form = q1_form)
 
 
 if __name__ == '__main__':
